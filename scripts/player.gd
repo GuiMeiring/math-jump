45d99575a4a1fa0ee6da122096a19f5511003d33
GuiMeiring <guilhermemeiring08@gmail.com>
2026-04-07 19:39:58 -0300

    feat: add enemy with actions and interactions

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,29 +5,22 @@ enum PlayerState {
 	walk,
 	jump,
 	fall,
-	duck
+	duck,
+	hurt
 }
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var max_speed = 100.0
-@export var acceleration = 100
-@export var deceleration = 100
+@export var acceleration = 400
+@export var deceleration = 400
 const JUMP_VELOCITY = -300.0
 
 var direction = 0
 var jump_count = 0
 @export var max_jump_count = 2
 var status: PlayerState
-
-func move(delta):
-	update_direction()
-	
-	if direction:
-		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
-	else:
-		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
 func _ready() -> void:
 	go_to_idle_state()
@@ -48,6 +41,8 @@ func _physics_process(delta: float) -> void:
 			fall_state(delta)
 		PlayerState.duck:
 			duck_state(delta)
+		PlayerState.hurt:
+			hurt_state(delta)
 	
 	move_and_slide()
 
@@ -72,14 +67,19 @@ func go_to_fall_state():
 func go_to_duck_state():
 	status = PlayerState.duck
 	anim.play("duck")
-	collision_shape.shape.radius = 5
-	collision_shape.shape.height = 10
-	collision_shape.position.y = 10
+	collision_shape.shape.radius = 12
+	collision_shape.shape.height = 30
+	collision_shape.position.y = 5
 	
 func exit_from_duck_state():
-	collision_shape.shape.radius = 15
-	collision_shape.shape.height = 30
+	collision_shape.shape.radius = 12
+	collision_shape.shape.height = 40
 	collision_shape.position.y = 0
+
+func go_to_hurt_state():
+	status = PlayerState.hurt
+	anim.play("hurt")
+	velocity = Vector2.ZERO
 
 func idle_state(delta):
 	move(delta)
@@ -136,12 +136,23 @@ func fall_state(delta):
 			go_to_walk_state()
 		return
 
-func duck_state(delta):
+func duck_state(_delta):
 	update_direction()
 	if Input.is_action_just_released("duck"):
 		exit_from_duck_state()
 		go_to_idle_state()
 		return
+
+func hurt_state(_delta):
+	pass
+
+func move(delta):
+	update_direction()
+	
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
 func update_direction():
 	direction = Input.get_axis("left", "right")
@@ -153,3 +164,12 @@ func update_direction():
 
 func can_jump() -> bool:
 	return jump_count < max_jump_count
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	if velocity.y > 0:
+		# inimigo morre
+		area.get_parent().take_damage()
+		go_to_jump_state()
+	else:
+		# player morre
+		go_to_hurt_state()
