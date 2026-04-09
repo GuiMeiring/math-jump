32a91ee5d0fd007255e818b7d1d0b945ac8fef84
GuiMeiring <guilhermemeiring08@gmail.com>
2026-04-08 21:39:12 -0300

    fix: remove bone after delay

--- scripts/spinning_bone.gd
+++ scripts/spinning_bone.gd
@@ -8,6 +8,9 @@ var direction = 1
 func _process(delta: float) -> void:
 	position.x += speed * delta * direction
 
-func set_direction(direction):
-	self.direction = direction
+func set_direction(skeleton_direction):
+	direction = skeleton_direction
 	anim.flip_h = direction < 0
+
+func _on_self_destruct_timer_timeout() -> void:
+	queue_free()
